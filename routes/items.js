0cbcf19928c9891c1a67c2c14c4e089e5acdfb10
boolfalse <boolfalse@mail.ru>
2022-07-09 18:36:44 +0400

    using reply item schemas

--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,45 @@
 
 const items = require('./../items.js');
 
+// reply options for getting all the items
+const getItemsOptions = {
+    schema: {
+        response: {
+            200: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        id: { type: 'number' },
+                        name: { type: 'string' }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// reply option for getting a single item
+const getItemOptions = {
+    schema: {
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    id: { type: 'number' },
+                    name: { type: 'string' }
+                }
+            }
+        }
+    }
+}
+
 function itemRoutes(fastify, options, done) {
-    fastify.get('/items', async (request, reply) => {
+    fastify.get('/items', getItemsOptions, async (request, reply) => {
         reply.send(items);
     });
 
-    fastify.get('/items/:id', async (request, reply) => {
+    fastify.get('/items/:id', getItemOptions, async (request, reply) => {
         const {id} = request.params;
         const item = items.find(item => item.id === parseInt(id));
 
